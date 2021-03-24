f58ce4bcd19da2e2661470d577de00f9d84bafeb
Danny <d.khorasanizadeh@ec.iut.ac.ir>
2021-03-24 19:29:56 +0430

    Initial commit

---
+++ Client.cpp
@@ -0,0 +1,14 @@
+/*
+--------------------------------------------------
+Name    : Ap Mini Project
+Author  : Danial Khorasanizadeh - Hadi Jaafari
+Course  : Advanced Programming - Spring 1400
+Date    : 24/Mar/2021
+--------------------------------------------------
+*/
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    return 0;
+}
