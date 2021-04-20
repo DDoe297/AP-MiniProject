120e35251672d0b2f22af2b8a8697fef87c53842
Danny <d.khorasanizadeh@ec.iut.ac.ir>
2021-04-21 02:58:51 +0430

    Added Showgame Thread

--- Temporary/Client.cpp
+++ Temporary/Client.cpp
@@ -1,5 +1,12 @@
-#include <vector>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <thread>
+int player, gameType, turn, remainingTime, winner, lastMessageType;
+char palyerChoice;
+std::string reciveData, sendData, errorMessage;
+std::vector<int> gameBase;
 char printBoardHelper(int player, char boardChar)
 {
     if (player == 1)
@@ -54,9 +61,76 @@ void showBoardThree(std::vector<int> gameBase)
         << "| /       \\ |" << std::endl
         << printBoardHelper(gameBase[19], 'T') << "-----------" << printBoardHelper(gameBase[20], 'U') << std::endl;
 }
+void showGame(void)
+{
+    boost::asio::streambuf buff;
+    boost::asio::read_until(sock, buff, "\n");
+    reciveData = std::string(boost::asio::buffer_cast<const char *>(buff.data()));
+    inputClient(reciveData);
+    system("cls");
+    std::cout << "Remaining Time:" << remainingTime << std::endl;
+    if (errorMessage.length())
+    {
+        std::cout << errorMessage << std::endl;
+    }
+    switch (gameType)
+    {
+    case 1:
+        showBoardOne(gameBase);
+        break;
+    case 2:
+        showBoardTwo(gameBase);
+        break;
+    case 3:
+        showBoardThree(gameBase);
+        break;
+    default:
+        break;
+    }
+    std::cout << ">";
+}
+void inputClient(std::string input)
+{
+    std::istringstream split(input);
+    std::string token;
+    std::getline(split, token, ':');
+    int dType;
+    dType = std::stoi(token);
+    if (dType == 1)
+    {
+        std::getline(split, token, ':');
+        player = stoi(token);
+    }
+    else if (dType == 2)
+    {
+        std::vector<int> gameBase;
+        std::getline(split, token, '-');
+        gameType = std::stoi(token);
+        std::getline(split, token, '-');
+        std::istringstream base(token);
+        std::string point;
+        while (std::getline(base, point, ','))
+        {
+            gameBase.push_back(stoi(point));
+        }
+        std::getline(split, token, '-');
+        turn = std::stoi(token);
+        std::getline(split, token, '-');
+        remainingTime = std::stoi(token);
+    }
+    else if (dType == 3)
+    {
+        std::getline(split, token, ':');
+        winner = stoi(token);
+    }
+}
+void getChoice(void)
+{
+    std::cin >> palyerChoice;
+}
 int main(void)
 {
-    std::vector<int> i = {0, 1, 1, 2, 2, 1, 0, 0, 2, 1, 2, 0, 2, 1, 1, 2, 0, 1, 2, 0, 1};
-    showBoardThree(i);
+    std::thread getInput(getChoice);
+    std::thread showOutput(showGame);
     return 0;
 }
--- include/client_connection.hpp
+++ include/client_connection.hpp
@@ -6,10 +6,9 @@
 
 
 
-
 void receive(boost::asio::ip::tcp::socket &sock);
 
-void send(boost ::asio::ip::tcp::socket &sock);
+void send(boost::asio::ip::tcp::socket &sock);
 
 void initConnections();
 
