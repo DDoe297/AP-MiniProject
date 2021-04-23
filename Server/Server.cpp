3965dc0f1ddfeb2eb7e5181bf22a6ef54c095774
hadi <hadidimad138@gmail.com>
2021-04-23 13:02:20 +0430

    last commit

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -6,7 +6,7 @@ Course  : Advanced Programming - Spring 1400
 Date    : 24/Mar/2021
 --------------------------------------------------
 */
-#include "../include/connection.hpp"
+#include "./include/connection.hpp"
 int main(void)
 {
     initConnections();
